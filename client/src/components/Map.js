cc41d7a73bc979eae24293c5e46c368dd825e89b
sotomaque <sotomaque@gmail.com>
2020-01-23 17:35:13 -0800

    added mapbox integration, configured app screen to show map as well as a pin for users current position

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
+import PinIcon from './PinIcon';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition()
+  }, []);
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setViewport({...viewport, latitude, longitude})
+        setUserPosition({latitude, longitude})
+      })
+    }
+  }
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1Ijoic290b21hcXVlIiwiYSI6ImNrNXI5MGlwYzAyMm0zbXBsbmhiaThxaWEifQ.RB-lTNtYCYvfkFG6CTbnAQ"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+           />
+        </div>
+
+        {/* Pin for Users Current Position */}
+        { userPosition && (
+            <Marker 
+              latitude={userPosition.latitude}
+              longitude={userPosition.longitude}
+              offsetLeft={-19}
+              offsetTop={-37}
+            >
+              <PinIcon size={40} color={'red'} />
+            </Marker>
+        )}
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: "flex",
+    position: "absolute",
+    top: 64,
   },
   rootMobile: {
     display: "flex",
